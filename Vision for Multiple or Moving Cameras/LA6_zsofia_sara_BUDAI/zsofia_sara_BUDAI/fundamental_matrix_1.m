033926856b48dd8a991e2fc6f514a1509857e6d6
budzs <61503191+budzs@users.noreply.github.com>
2023-09-17 12:56:24 +0200

    Add files via upload

---
+++ Vision for Multiple or Moving Cameras/LA6_zsofia_sara_BUDAI/zsofia_sara_BUDAI/fundamental_matrix_1.m
@@ -0,0 +1,27 @@
+% Exercises based on the fundamental matrix
+% Ex1: Visualize the relations between points and epipolar lines in two
+% images using the vgg-mvg toolbox
+
+clear
+close all
+
+% include vgg-mvg path (modify for your own path to the vgg-mvg toolbox)
+vgg_mvg_path = 'D:\ITK\Msc\IPCV\II félév\VMMC\LAVI\allfns\';
+addpath(genpath(vgg_mvg_path));
+
+% Compute F from two camera matrices using vgg_F_from_P and display the result
+% P's are read from stored example in vgg_example_scene
+
+% Read in P's and images
+[view] = vgg_example_scene(2);
+
+% Compute F from P's
+F = vgg_F_from_P(view(1).P, view(2).P);
+% rank_of_F = rank(F)
+
+% Display
+vgg_gui_F(view(1).I, view(2).I, F');
+disp('Computed epipolar geometry. Move the mouse to verify')
+
+[x_coord, y_coord]=ginput(2);
+
